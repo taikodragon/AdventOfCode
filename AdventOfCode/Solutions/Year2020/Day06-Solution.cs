8557cab5615da78c2c19b202cdd2a24df5e89721
Thomas Luckart <taikodragon@hotmail.com>
2020-12-05 21:15:33 -0800

    2020 Day 06

--- AdventOfCode/Solutions/Year2020/Day06-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06-Solution.cs
@@ -8,20 +8,54 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day06 : ASolution
     {
+        List<string> lines = new List<string>();
 
         public Day06() : base(06, 2020, "")
         {
             UseDebugInput = false;
+
+            lines = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            List<string> groups = new List<string>();
+            string thisGroup = string.Empty;
+            foreach(string ga in lines) {
+                if( ga == string.Empty ) {
+                    groups.Add(thisGroup);
+                    thisGroup = string.Empty;
+                    continue;
+                }
+                foreach( char a in ga ) {
+                    if( !thisGroup.Contains(a) ) {
+                        thisGroup = string.Concat(thisGroup, a);
+                    }
+                }
+            }
+            if(thisGroup != string.Empty) {
+                groups.Add(thisGroup);
+            }
+            return groups.Select(s => s.Length).Sum().ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            List<string> groups = new List<string>();
+            string thisGroup = null;
+            foreach( string ga in lines ) {
+                if( ga == string.Empty ) {
+                    groups.Add(thisGroup);
+                    thisGroup = null;
+                    continue;
+                }
+                if( thisGroup == null ) thisGroup = ga;
+                else thisGroup = string.Concat(thisGroup.Where(c => ga.Contains(c)));
+            }
+            if( thisGroup != string.Empty ) {
+                groups.Add(thisGroup);
+            }
+            return groups.Select(s => s.Length).Sum().ToString();
         }
     }
 }
