88a60c8478d1abed7e290af899cacec33336e7cb
Thomas Luckart <taikodragon@hotmail.com>
2020-12-07 21:41:39 -0800

    2020 Day 08 -- Create AsmProcessor

---
+++ AdventOfCode/Solutions/Year2020/AsmProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    enum Operation
+    {
+        nop, acc, jmp
+    }
+    class Instruction
+    {
+        public Instruction() => Op = Operation.nop;
+        public Instruction(string line)
+        {
+            string[] parts = line.Split(' ');
+            Op = (Operation)Enum.Parse(typeof(Operation), parts[0]);
+            Arg = int.Parse(parts[1].Replace("+", string.Empty));
+        }
+        public Operation Op { get; set; }
+        public long Arg { get; set; }
+    }
+    class AsmProcessor
+    {
+        public static List<Instruction> ParseProgram(List<string> programLines)
+        {
+            return programLines.Select(l => new Instruction(l)).ToList();
+        }
+
+        public long Run(List<Instruction> program, out bool didFault) {
+            List<int> pcVisited = new List<int>();
+
+            long accumulator = 0;
+            for(int pc = 0; pc < program.Count;) {
+                if( pcVisited.Contains(pc) ) {
+                    didFault = true;
+                    return accumulator;
+                }
+                else pcVisited.Add(pc);
+                Instruction inst = program[pc];
+
+                switch(inst.Op) {
+                    case Operation.nop:
+                        pc++;
+                        break;
+                    case Operation.acc:
+                        accumulator += inst.Arg;
+                        pc++;
+                        break;
+                    case Operation.jmp:
+                        pc += (int)inst.Arg;
+                        break;
+                    default:
+                        throw new Exception("Invalid Op");
+                }
+            }
+            didFault = false;
+            return accumulator;
+        }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day08-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day08-Solution.cs
@@ -8,20 +8,37 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day08 : ASolution
     {
-
-        public Day08() : base(08, 2020, "")
+        AsmProcessor pc = new AsmProcessor();
+        List<string> lines;
+        public Day08() : base(08, 2020, "Handheld Halting")
         {
             UseDebugInput = false;
+
+            lines = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            bool didFault;
+            return pc.Run(AsmProcessor.ParseProgram(lines), out didFault).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int modPos = 0;
+            bool didFault = true;
+            long acc;
+            List<Instruction> program = AsmProcessor.ParseProgram(lines);
+            do {
+                acc = pc.Run(program, out didFault);
+                if( didFault ) {
+                    program = AsmProcessor.ParseProgram(lines);
+                    int nextMod = program.FindIndex(modPos, i => (i.Op == Operation.nop && i.Arg != 0) || i.Op == Operation.jmp);
+                    modPos = nextMod + 1;
+                    program[nextMod].Op = program[nextMod].Op == Operation.nop ? Operation.jmp : Operation.nop;
+                }
+            } while( didFault );
+            return acc.ToString();
         }
     }
 }
