ec3d5a8fdfe91a0aab4b0a4b7d393f4fd7a19ee3
Thomas Luckart <taikodragon@hotmail.com>
2020-12-13 21:52:52 -0800

    2020 Day 14

--- AdventOfCode/Solutions/Year2020/Day14-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day14-Solution.cs
@@ -8,20 +8,105 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day14 : ASolution
     {
-
+        readonly List<(string cmd, string value)> lines;
         public Day14() : base(14, 2020, "")
         {
             UseDebugInput = false;
+
+            lines = Input
+                .Replace("mem[", string.Empty)
+                .Replace("]", string.Empty)
+                .SplitByNewline()
+                .Select(l => {
+                    var pair = l.Split('=');
+                    return (pair[0].Trim(), pair[1].Trim());
+                }).ToList();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            Dictionary<int, ulong> memory = new Dictionary<int, ulong>();
+            string mask = string.Empty;
+            int maskLength = 0;
+            foreach(var inst in lines) {
+                if(inst.cmd == "mask") {
+                    mask = inst.value;
+                    maskLength = mask.Length - 1;
+                    continue;
+                }
+                int memAddr = int.Parse(inst.cmd);
+                ulong newValue = ulong.Parse(inst.value);
+                
+                // do the zeros
+                for(int i = maskLength; i >= 0; --i) {
+                    int shift = maskLength - i;
+                    if( mask[i] == '0') {
+                        newValue &= (~(0x1ul << shift));
+                    }
+                    if( mask[i] == '1' ) {
+                        newValue |= (0x1ul << shift);
+                    }
+                }
+                memory[memAddr] = newValue;
+            }
+
+            ulong sum = 0;
+            foreach( var p in memory ) sum += p.Value;
+            return sum.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            Dictionary<ulong, ulong> memory = new Dictionary<ulong, ulong>();
+            List<string> masks = new List<string>();
+            int maskLength = 0;
+            foreach( var inst in lines ) {
+                if( inst.cmd == "mask" ) {
+                    masks.Clear();
+                    maskLength = inst.value.Length - 1;
+                    Queue<string> mp = new Queue<string>();
+                    mp.Enqueue(inst.value);
+                    while(mp.Count > 0) {
+                        string mask = mp.Dequeue();
+                        bool didQueue = false;
+                        for( int i = mask.Length - 1; i >= 0; --i ) {
+                            if(mask[i] == 'X') {
+                                string m1 = mask.Substring(0, i),
+                                    m2 = mask.Substring(i+1, (mask.Length-1) - i);
+                                mp.Enqueue(m1 + "Z" + m2);
+                                mp.Enqueue(m1 + "1" + m2);
+                                didQueue = true;
+                                break;
+                            }
+                        }
+                        if(!didQueue) {
+                            masks.Add(mask);
+                        }
+                    }
+                    continue;
+                }
+                ulong memAddrOri = ulong.Parse(inst.cmd);
+                ulong newValue = ulong.Parse(inst.value);
+
+                foreach( string mask in masks ) {
+                    ulong memAddr = memAddrOri;
+                    for( int i = maskLength; i >= 0; --i ) {
+                        int shift = maskLength - i;
+                        if( mask[i] == 'Z' ) {
+                            memAddr &= (~(0x1ul << shift));
+                        }
+                        if( mask[i] == '1' ) {
+                            memAddr |= (0x1ul << shift);
+                        }
+                    }
+                    memory[memAddr] = newValue;
+                }
+            }
+
+            ulong sum = 0;
+            foreach( var p in memory ) sum += p.Value;
+            return sum.ToString();
         }
     }
 }
+
