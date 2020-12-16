30417d9be6ccae7c05bca955ba5db51ff1519244
Thomas Luckart <taikodragon@hotmail.com>
2020-12-15 22:41:13 -0800

    2020 Day 16 cleanup

--- AdventOfCode/Solutions/Year2020/Day16-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day16-Solution.cs
@@ -52,33 +52,31 @@ namespace AdventOfCode.Solutions.Year2020
             List<string> groups = Input.Split("\n\n").ToList();
             List<Rule> rules = new List<Rule>();
 
-            foreach( string line in groups[0].SplitByNewline() ) {
-                var parts = line.Split(new string[] { ": ", " or ", "-" }, StringSplitOptions.RemoveEmptyEntries);
-                rules.Add(new Rule {
+            groups[0].SplitByNewline()
+                .Select(line => line.Split(new string[] { ": ", " or ", "-" }, StringSplitOptions.RemoveEmptyEntries))
+                .ToList()
+                .ForEach(parts => rules.Add(new Rule {
                     Label = parts[0],
                     RangeLow = new IntCoord(int.Parse(parts[1]), int.Parse(parts[2])),
                     RangeHigh = new IntCoord(int.Parse(parts[3]), int.Parse(parts[4]))
-                });
-            }
+                }));
 
-
-            var otherInt = groups[2]
+            var others = groups[2]
                 .SplitByNewline()
                 .Skip(1)
                 .Select(s => s.Split(',').Select(n => int.Parse(n)).ToList())
-                .ToList();
-            var others = otherInt
                 .Where(l => l.TrueForAll(n => rules.Any(r => r.IsValidInRange(n))))
                 .ToList();
 
             int tickLength = rules.Count;
-            Dictionary<int, Rule> foundRules = new Dictionary<int, Rule>();
+            Dictionary<int, Rule> foundRules = new Dictionary<int, Rule>(20);
 
             List<int> vals = null;
             Func<Rule, bool> ruleTest = rule => vals.TrueForAll(n => rule.IsValidInRange(n));
             while(foundRules.Count < tickLength) {
                 for(int i = 0; i < tickLength; ++i) {
                     if( foundRules.ContainsKey(i) ) continue;
+
                     vals = others.Select(l => l[i]).ToList();
 
                     if( rules.Count(ruleTest) == 1) {
@@ -90,7 +88,8 @@ namespace AdventOfCode.Solutions.Year2020
             }
 
             var mine = groups[1]
-                .SplitByNewline().Skip(1)
+                .SplitByNewline()
+                .Skip(1)
                 .Select(s => s.Split(',').Select(n => int.Parse(n)).ToList())
                 .First();
 
