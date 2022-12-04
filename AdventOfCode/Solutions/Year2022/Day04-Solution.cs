89900f8b13e237a593640119c7f1386048206031
Thomas Luckart <taikodragon@hotmail.com>
2022-12-03 23:40:09 -0800

    Day 4 2022

--- AdventOfCode/Solutions/Year2022/Day04-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day04-Solution.cs
@@ -1,3 +1,4 @@
+using ABI.System.Collections.Generic;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
@@ -10,19 +11,49 @@ namespace AdventOfCode.Solutions.Year2022;
 [DayInfo(2022, 04, "")]
 class Day04 : ASolution
 {
+    class SectionAssignment {
+        public SectionAssignment(string range) {
+            var numbers = range.Split('-');
+            if (numbers.Length != 2) throw new ArgumentException("Unknown range sequence");
+            Lower = int.Parse(numbers[0]);
+            Upper = int.Parse(numbers[1]);
+        }
+        public int Lower { get; set; }
+        public int Upper { get; set; }
+
+        public bool FullContains(SectionAssignment other) {
+            return Lower <= other.Lower && Upper >= other.Upper;
+        }
+        public bool PartialContains(SectionAssignment other) {
+            return FullContains(other) ||
+                (Lower <= other.Lower && other.Lower <= Upper) ||
+                (Lower <= other.Upper && other.Upper <= Upper);
+        }
+    }
 
     public Day04() : base(false)
     {
         
     }
 
+    List<(SectionAssignment Elf1, SectionAssignment Elf2)> data = new();
+    protected override void ParseInput() {
+        data.AddRange(
+            Input.SplitByNewline(false, true)
+                .Select(s => s.Split(','))
+                .Select(pair => (new SectionAssignment(pair[0]), new SectionAssignment(pair[1])))
+        );
+    }
+
     protected override string SolvePartOne()
     {
-        return null;
+        return data.Count(pair => pair.Elf1.FullContains(pair.Elf2) || pair.Elf2.FullContains(pair.Elf1))
+            .ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return null;
+        return data.Count(pair => pair.Elf1.PartialContains(pair.Elf2) || pair.Elf2.PartialContains(pair.Elf1))
+            .ToString();
     }
 }
