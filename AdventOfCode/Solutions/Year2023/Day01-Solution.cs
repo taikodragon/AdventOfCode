12129ab59dc076618f6e01fb5c0e9714dfb789a2
Thomas Luckart <taikodragon@hotmail.com>
2023-11-30 22:02:15 -0800

    2023 Day 1 Polished

--- AdventOfCode/Solutions/Year2023/Day01-Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day01-Solution.cs
@@ -8,7 +8,7 @@ using System.Threading;
 
 namespace AdventOfCode.Solutions.Year2023;
 
-[DayInfo(2023, 01, "")]
+[DayInfo(2023, 01, "Trebuchet?!")]
 class Day01 : ASolution
 {
 
@@ -26,29 +26,27 @@ class Day01 : ASolution
     {
         return Input.SplitByNewline(false, true)
             .Select(s => s.Where(char.IsDigit).ToArray())
-            .Where(arr => arr.Length >= 2)
-            .Select(arr => $"{arr.First()}{arr.Last()}")
-            .Select(int.Parse)
+            .Where(arr => arr.Length >= 1)
+            .Select(arr => (arr.First() - '0') * 10 + (arr.Last() - '0'))
             .Sum();
     }
 
     protected override object SolvePartTwoRaw()
     {
         string[] numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-        int[] numberMap = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         int sum = 0;
         foreach(string line in Input.SplitByNewline(false, true)) {
             List<int> lineNums = [];
 
             for(int i = 0; i < line.Length; i++) {
-                if (char.IsDigit(line[i])) lineNums.Add(int.Parse(line[i].ToString()));
+                if (char.IsDigit(line[i])) lineNums.Add(line[i] - '0'); // '1' - '0' = 1 because '0' is 48 and '1' is 49
                 else {
                     for(int np = 0; np < numbers.Length; np++) {
                         string num = numbers[np];
-                        if (i + num.Length > line.Length) continue;
-                        if ( num == line[i..(i+num.Length)] ) {
-                            lineNums.Add(numberMap[np]);
+                        if ( line[i..].StartsWith(num) ) {
+                            lineNums.Add(np + 1); // shift index to one base
+                            i += num.Length - 2; // jump over the content of the matched number
                             break;
                         }
                     }
