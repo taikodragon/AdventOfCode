0aec1534384143e5cc9f615ebe2bf8e9898a7d7c
Thomas Luckart <taikodragon@hotmail.com>
2023-12-03 23:03:20 -0800

    2023 Day 4 - No race

--- AdventOfCode/Solutions/Year2023/Day04-Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day04-Solution.cs
@@ -7,27 +7,65 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2023;
 
-[DayInfo(2023, 04, "")]
+[DayInfo(2023, 04, "Scratchcards")]
 class Day04 : ASolution
 {
-
-    public Day04() : base(true)
+    class Scratchcard
     {
-            
+        public int Id;
+        public HashSet<int> winning = new();
+        public List<int> yourNumbers = new();
+        public int WorthPoints;
+        public int WinCount;
     }
 
-    protected override void ParseInput()
-    {
+
+    public Day04() : base(false) {
 
     }
 
-    protected override object SolvePartOneRaw()
-    {
-        return null;
+    List<Scratchcard> cards = new();
+    protected override void ParseInput() {
+        foreach (string line in Input.SplitByNewline()) {
+            var numbers = line.Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+            Scratchcard card = new() {
+                Id = int.Parse(numbers[1])
+            };
+            bool foundSplit = false;
+            foreach (var numStr in numbers[2..]) {
+                if (numStr == "|") {
+                    foundSplit = true;
+                    continue;
+                }
+
+                if (foundSplit) card.yourNumbers.Add(int.Parse(numStr));
+                else card.winning.Add(int.Parse(numStr));
+            }
+            int winCount = card.WinCount = card.yourNumbers.Intersect(card.winning).Count();
+            card.WorthPoints = winCount > 0 ? (int)Math.Pow(2, winCount - 1) : 0;
+            cards.Add(card);
+        }
     }
 
-    protected override object SolvePartTwoRaw()
-    {
-        return null;
+    protected override object SolvePartOneRaw() {
+        return cards.Sum(c => c.WorthPoints);
+    }
+
+    protected override object SolvePartTwoRaw() {
+        Dictionary<int, int> copyCount = new();
+
+        cards.ForEach(c => copyCount.Add(c.Id, 1));
+
+        int visited = 0;
+        foreach (var card in cards) {
+            int myCopies = copyCount[card.Id];
+            visited += myCopies;
+            // Add copies
+            for (int i = 1; i <= card.WinCount; i++) {
+                copyCount[card.Id + i] += myCopies;
+            }
+        }
+
+        return visited;
     }
 }
